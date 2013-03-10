4da4327c9215f7a1ebed837ee5470622d928a56e
stephen soltesz <stephen.soltesz@gmail.com>
2013-03-10 01:18:32 +0000

    change print format

--- lib.cpp
+++ lib.cpp
@@ -54,7 +54,7 @@ int send_data(tcp::socket& socket, int t_length)
             //std::cout << (t2-t1) << " " << t_length << " " << std::endl;
             //status(tdiff, h_sent);
             fprintf(stdout, "status: %.6f\n", tdiff);
-            fprintf(stdout, "status: %ld\n", h_sent);
+            fprintf(stdout, "status: %zd\n", h_sent);
             last_status = tdiff;
         }
     }
@@ -80,7 +80,7 @@ int recv_data(tcp::socket& socket, int t_length) {
     while ( (t2-t1) < t_length ) {
         //ret = recv(sd, data, recvd_length , 0);
         ret = socket.receive(boost::asio::buffer(data), 0, error);
-        fprintf(stdout, "ret==%ld\n", ret);
+        fprintf(stdout, "ret==%zd\n", ret);
         if ( ret == 0 ) {
             fprintf(stdout, "ret==0\n");
             if ( error ) {
@@ -88,14 +88,14 @@ int recv_data(tcp::socket& socket, int t_length) {
             }
             break;
         }
-        fprintf(stdout, "h_recvd: %ld\n", h_recvd);
+        fprintf(stdout, "h_recvd: %zd\n", h_recvd);
         h_recvd = ret + h_recvd;
         t2 = get_ts();
         tdiff = t2-t1;
         if ( last_status + 1 < tdiff ) {
             //status(tdiff, h_recvd);
             fprintf(stdout, "status: %.6f\n", tdiff);
-            fprintf(stdout, "status: %ld\n", h_recvd);
+            fprintf(stdout, "status: %zd\n", h_recvd);
             last_status = tdiff;
         }
     }
