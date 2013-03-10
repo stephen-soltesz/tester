4b2f296d964a3c99d3d179861c38a258bcf2d40e
stephen soltesz <stephen.soltesz@gmail.com>
2013-03-10 05:25:49 +0000

    add boost timing functions

--- lib.cpp
+++ lib.cpp
@@ -3,14 +3,26 @@
 #include <boost/asio.hpp>
 #include <sys/time.h>
 #include <stdio.h>
+#include <boost/date_time.hpp>
 
+using namespace boost::posix_time;
 using boost::asio::ip::tcp;
 #include "lib.hpp"
 
+ptime get_pts() {
+    return microsec_clock::local_time();
+}
+
+double get_diff(ptime t1, ptime t2) {
+    time_duration td = t2-t1;
+    return td.total_microseconds()/1.0e6;
+}
+
 double get_ts() {
-    struct timeval tv1; 
+/*    struct timeval tv1; 
     gettimeofday(&tv1, NULL);
-    return (tv1.tv_sec + tv1.tv_usec/1.0e6);
+    return (tv1.tv_sec + tv1.tv_usec/1.0e6); */
+    return 0;
 } 
 
 void status(double tdiff, std::size_t amount) {
@@ -20,7 +32,7 @@ void status(double tdiff, std::size_t amount) {
 
 std::size_t send_data(tcp::socket& socket, int t_length)
 {
-    double      t1,t2;
+    ptime       t1,t2;
     double      tdiff=0;
     double      last_status = 0;
     std::size_t h_sent=0;
@@ -29,12 +41,12 @@ std::size_t send_data(tcp::socket& socket, int t_length)
     boost::array<char, DATASIZE>    data;
     boost::system::error_code       error;
 
-    t1 = get_ts();
+    t1 = get_pts();
     t2 = t1;
     h_sent = 0;
 
     fprintf(stdout, "requested: %d\n", t_length);
-    while ( (t2-t1) < t_length ) {
+    while ( get_diff(t1,t2) < t_length ) {
         ret = socket.send(boost::asio::buffer(data), 0, error);
         if ( ret == 0 ) {
             perror("sending error:");
@@ -45,8 +57,8 @@ std::size_t send_data(tcp::socket& socket, int t_length)
             break;
         }
         h_sent += ret;
-        t2 = get_ts();
-        tdiff = t2-t1;
+        t2 = get_pts();
+        tdiff = get_diff(t1,t2);
         if ( last_status + 1 < tdiff ) {
             status(tdiff, h_sent);
             last_status = tdiff;
@@ -57,7 +69,7 @@ std::size_t send_data(tcp::socket& socket, int t_length)
 }
 
 std::size_t recv_data(tcp::socket& socket, int t_length) {
-    double      t1,t2;
+    ptime       t1,t2;
     double      tdiff=0;
     double      last_status = 0;
 
@@ -67,12 +79,12 @@ std::size_t recv_data(tcp::socket& socket, int t_length) {
     boost::array<char, DATASIZE> data;
     boost::system::error_code error;
 
-    t1 = get_ts();
+    t1 = get_pts();
     t2 = t1;
     h_recvd = 0;
 
     fprintf(stdout, "requested: %d\n", t_length);
-    while ( (t2-t1) < t_length ) {
+    while ( get_diff(t1,t1) < t_length ) {
         ret = socket.receive(boost::asio::buffer(data), 0, error);
         if ( ret == 0 ) {
             fprintf(stdout, "ret==0\n");
@@ -82,8 +94,8 @@ std::size_t recv_data(tcp::socket& socket, int t_length) {
             break;
         }
         h_recvd = ret + h_recvd;
-        t2 = get_ts();
-        tdiff = t2-t1;
+        t2 = get_pts();
+        tdiff = get_diff(t1, t2);
         if ( last_status + 1 < tdiff ) {
             status(tdiff, h_recvd);
             last_status = tdiff;
