632d0102c1c45329608d8cdf0bb6861186d2fd0c
stephen soltesz <stephen.soltesz@gmail.com>
2013-03-10 05:26:15 +0000

    add posix time functions

--- lib.hpp
+++ lib.hpp
@@ -1,4 +1,6 @@
 
+boost::posix_time::ptime get_pts(void);
+double get_diff(boost::posix_time::ptime t1, boost::posix_time::ptime t2);
 double get_ts(void);
 std::size_t send_data(tcp::socket& socket, int t_length);
 std::size_t recv_data(tcp::socket& socket, int t_length);
